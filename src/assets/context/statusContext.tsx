32a3effad8e55b4b3c7e8770e917c11102049ab1
furquannawab <94888920+furquannawab@users.noreply.github.com>
2022-01-10 23:36:08 +0530

    Update statusContext.tsx

--- src/assets/context/statusContext.tsx
+++ src/assets/context/statusContext.tsx
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 const statusContext = createContext({
   status: "reading",
-  setStatus: (status: string) => {},
+  setStatus: (status: string) => undefined,
 });
 
 export default statusContext;
